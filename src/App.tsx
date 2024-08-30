d295887886242d843121cd74495ba3e7876c05e8
Andrii <whiteyelnikov@gmail.com>
2024-08-30 19:50:43 +0300

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+import Table from './components/Table';
+import Header from './components/Header';
 
 function App() {
+  const [data, setData] = useState([]);
+
+  async function getDataFromServer() {
+    const response = await fetch('https://jsonplaceholder.typicode.com/users');
+    const data = await response.json();
+    setData(data);
+  }
+
+  useEffect(() => {
+    getDataFromServer();
+  }, []);
+  console.log(data)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header />
+      <div className="container">
+        <h1>
+          List of users:
+        </h1>
+        <Table
+          data={data}
+        />
+      </div>
+    </>
   );
 }
 
---
+++ src/components/Table.tsx
@@ -0,0 +1,36 @@
+import * as React from 'react';
+import { DataGrid, GridColDef } from '@mui/x-data-grid';
+import Paper from '@mui/material/Paper';
+
+const columns: GridColDef[] = [
+    { field: 'id', headerName: 'ID', width: 70 },
+    { field: 'name', headerName: 'Name', width: 130 },
+    { field: 'username', headerName: 'Username', width: 110 },
+    {
+        field: 'email',
+        headerName: 'Email',
+        width: 240,
+    },
+    {
+        field: 'phone',
+        headerName: 'Phone',
+        width: 150
+    },
+];
+
+const paginationModel = { page: 0, pageSize: 5 };
+
+export default function Table({ data }: { data: any }) {
+    return (
+        <Paper sx={{ height: 400, width: '100%' }}>
+            <DataGrid
+                rows={data}
+                columns={columns}
+                initialState={{ pagination: { paginationModel } }}
+                pageSizeOptions={[5, 10]}
+                checkboxSelection
+                sx={{ border: 0 }}
+            />
+        </Paper>
+    );
+}
